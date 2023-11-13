4d5995dfd72f694a62a8b6deb82d7102e758fb54
mmcurry17 <mmcurry17@gmail.com>
2023-11-13 13:11:28 -0800

    function 5 commit

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 function play() {
   let secretNumber = 17;
+  let guessCounter = 0;
+  let guessLogger = [];
+  const user = prompt(`Enter your username`);
 
   while (true) {
-    const guess = prompt("Guess a number.");
+    const guess = prompt(`Guess a number ${user}.`);
+    guessCounter++;
+    guessLogger.push(guess);
+
     if (guess === null) {
-      alert("Goodbye!");
+      alert(`Goodbye ${user}!`);
       break;
     }
-    if (guess == secretNumber) {
-      alert("You are RIGHT!");
+    if (guess == secretNumber && guessCounter > 1) {
+      guessLogger.pop();
+      alert(
+        `You are RIGHT!, it took you ${guessCounter} tries.\nYour previous guesses were ${guessLogger}.`
+      );
       break;
+    }
+    if (guess == secretNumber && guessCounter === 1) {
+      alert(`You are RIGHT, you got it on the first try!`);
+      break;
+    }
+    if (guess < secretNumber && guess.length) {
+      alert(`Guess again ${user}, try a LARGER number.`);
+    }
+    if (guess > secretNumber) {
+      alert(`Guess again ${user}, try a SMALLER number.`);
     }
   }
 }
